fa1f943758cf701b62f1772fc14fc375fee9dd7d
hrantgartley <doyouevenjump@gmail.com>
2024-01-17 17:43:05 -0600

    match better

--- index.php
+++ index.php
@@ -6,14 +6,15 @@
     for ($i = 1; $i < 101; $i++) {
         $output = "";
 
-        if ($i % 3 == 0) {
-            $output .= "Blue ";
-        }
-        if ($i % 5 == 0) {
-            $output .= "Berry ";
-        }
         if ($i % 15 == 0) {
-            $output .= "Blueberry";
+            $output = "Blueberry";
+        } else {
+            if ($i % 3 == 0) {
+                $output .= "Blue ";
+            }
+            if ($i % 5 == 0) {
+                $output .= "Berry ";
+            }
         }
 
         echo $i . ($output ? " " . $output : "") . "<br>";
